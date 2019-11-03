66deb8307858ff79bc492a5289d8ae77028e5a7c
yuquimura <brianzambellitello@gmail.com>
2019-11-03 18:03:32 -0300

    Add of receive_input() method for Font Signed-off-by: yuquimura <brianzambellitello@gmail.com>
    Signed-off-by: yuquimura <brianzambellitello@gmail.com>

--- client/rendering/interfaces/Font.h
+++ client/rendering/interfaces/Font.h
@@ -13,11 +13,12 @@ class Font {
   TTF_Font *font;
   LTexture promptTextTexture;
   LTexture inputTextTexture;
-  SDL_Rect area;
+  SDL_Rect areaPromt;
+  SDL_Rect areaInput;
  public:
   Font(const std::string &msg, SDL_Renderer *renderer);
   void render(SDL_Renderer *p_renderer);
-
+  void receive_input(SDL_Event e);
 };
 
 #endif //MICROMACHINES_CLIENT_RENDERING_INTERFACES_FONT_H_
