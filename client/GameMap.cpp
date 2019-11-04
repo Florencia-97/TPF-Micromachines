96602dcc51983b329baa095467fe725faedebdbe
Florencia-97 <florrr1997@gmail.com>
2019-11-03 22:06:05 -0300

    Now layers load at once and they have h and w

--- client/GameMap.cpp
+++ client/GameMap.cpp
@@ -1,40 +1,47 @@
 #include "GameMap.h"
 #include "tiles/TilesFactory.h"
+#include "../common/MapsLayer.h"
 
 void GameMap::loadMap(const std::string &mapPath, SDL_Renderer *gRenderer) {
-  YAML::Node config = YAML::LoadFile(mapPath);
-  YAML::Node mapYaml = config["Map"];
-  int y = 0;
-  int x = 0;
-  int rowTile = -1;
-  int num = 1;
-  height = 0;
-  width = 0;
 
-  for (YAML::iterator it = mapYaml.begin(); it != mapYaml.end(); ++it) {
-    rowTile += 1;
-    x = 0;
+    MapsLayer mapsLayer(mapPath);
+    // You can acces to each layer as the attr is public.
+    // Now it has height and width
 
-    int columnTile = 0;
-    const YAML::Node &row = *it;
-    std::string numberRow = std::to_string(num);
-    this->map.emplace_back();
-    YAML::Node column = row[numberRow];
-    int colNum = 0;
-    width +=512;
+    YAML::Node config = YAML::LoadFile(mapPath);
+    YAML::Node mapYaml = config["Road"];
+    int y = 0;
+    int x = 0;
+    int rowTile = -1;
+    int num = 1;
+    // TODO: load real h and m with mapsLayer
+    height = 0;
+    width = 0;
+
+    for (YAML::iterator it = mapYaml.begin(); it != mapYaml.end(); ++it) {
+        rowTile += 1;
+        x = 0;
+
+        int columnTile = 0;
+        const YAML::Node &row = *it;
+        std::string numberRow = std::to_string(num);
+        this->map.emplace_back();
+        YAML::Node column = row[numberRow];
+        int colNum = 0;
+        width +=512;
 
     for (YAML::iterator c = column.begin(); c != column.end(); ++c) {
-      y = rowTile * 512;
-      x = columnTile * 512;
-      const YAML::Node &col_value = *c;
-      auto tile = tiles_factory.getTile(col_value.as<int>(), x, y, gRenderer);
-      this->map.back().push_back(tile);
+        y = rowTile * 512;
+        x = columnTile * 512;
+        const YAML::Node &col_value = *c;
+        auto tile = tiles_factory.getTile(col_value.as<int>(), x, y, gRenderer);
+        this->map.back().push_back(tile);
 
-      colNum++;
-      columnTile += 1;
-      height += 512;
+        colNum++;
+        columnTile += 1;
+        height += 512;
     }
-    
+
     num++;
   }
 }
--- common/MapsLayer.h
+++ common/MapsLayer.h
@@ -5,8 +5,15 @@
 #include <vector>
 
 class MapsLayer {
+    void _loadMap(const std::string &mapPath, std::string& key, std::vector<std::vector<int>>& matrix);
+    void _loadHW(const std::string &mapPath);
 public:
-    static std::vector<std::vector<int>> getLayerN(std::string& mapPath, int n);
+    std::vector<std::vector<int>> ground;
+    std::vector<std::vector<int>> road;
+    std::vector<std::vector<int>> extras;
+    int width;
+    int height;
+    MapsLayer(const std::string& mapPath);
 };
 
 
