8cde5de117e7ffac9c03345537a378130594ebc5
Florencia-97 <florrr1997@gmail.com>
2019-11-12 22:45:03 -0300

    We know can run lua in our project

---
+++ lua/fake_player.lua
@@ -0,0 +1,6 @@
+function decide_move(v1)
+    for i = 1, #v1 do
+        print(v1[i])
+    end
+    return "move left bro"
+end
---
+++ lua53/CMakeFiles/LuaLib.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lapi.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lapi.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lauxlib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lauxlib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lbaselib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lbaselib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lbitlib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lbitlib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lcode.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lcode.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lcorolib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lcorolib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lctype.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lctype.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/ldblib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/ldblib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/ldebug.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/ldebug.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/ldo.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/ldo.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/ldump.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/ldump.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lfunc.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lfunc.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lgc.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lgc.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/linit.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/linit.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/liolib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/liolib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/llex.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/llex.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lmathlib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lmathlib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lmem.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lmem.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/loadlib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/loadlib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lobject.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lobject.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lopcodes.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lopcodes.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/loslib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/loslib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lparser.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lparser.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lstate.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lstate.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lstring.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lstring.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lstrlib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lstrlib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/ltable.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/ltable.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/ltablib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/ltablib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/ltm.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/ltm.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lundump.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lundump.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lutf8lib.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lutf8lib.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lvm.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lvm.c.o"
+  "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/src/lzio.c" "/home/flox/FIUBA/Taller/TP4/TPF-Micromachines/lua53/CMakeFiles/LuaLib.dir/src/lzio.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "server"
+  "server/Game"
+  "server/Game/Box2D"
+  "lua"
+  "lua53/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
