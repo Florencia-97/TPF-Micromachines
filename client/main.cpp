8578947659cd3f5e11abcdde2d7fcdb293094344
Florencia-97 <florrr1997@gmail.com>
2019-11-13 23:42:48 -0300

    Arrange print in mains usage

--- client/main.cpp
+++ client/main.cpp
@@ -3,13 +3,13 @@
 // ./Client service port
 int main(int argc, char *args[]) {
     try {
-        if (argc != 3) throw std::runtime_error("error! use ./Client [HOST] [PORT]");;
+        if (argc != 3) throw std::runtime_error("error! use ./Client [HOST] [PORT]\n");;
         std::string service = args[1];
         std::string port = args[2];
         Client client(service, port);
         return client.play();
     } catch (...) {
-        std::cout << "Exception unknown occurred";
+        std::cout << "Exception unknown occurred\n";
         return 1;
     }
 }
--- server/main.cpp
+++ server/main.cpp
@@ -5,12 +5,12 @@
 // ./Server port
 int main (int argc, char** argv) {
     try {
-        if (argc != 2) throw std::runtime_error("no port was specified! use ./Server [PORT]");
+        if (argc != 2) throw std::runtime_error("no port was specified! use ./Server [PORT]\n");
         Server server;
         std::string port = argv[1];
         return server.play(port);
     } catch (...) {
-        std::cout << "Exception unknown occurred";
+        std::cout << "Exception unknown occurred\n";
         return 1;
     }
 }
