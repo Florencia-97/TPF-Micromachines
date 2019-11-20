8d1e3479e15a7a759eaa9da754922ac08e13e890
yuquimura <brianzambellitello@gmail.com>
2019-11-20 18:53:18 -0300

    Identation and documentation
    Signed-off-by: yuquimura <brianzambellitello@gmail.com>

--- client/rendering/interfaces/Animation.h
+++ client/rendering/interfaces/Animation.h
@@ -12,11 +12,19 @@ class Animation {
  private:
   LTexture actualSprite;
   int frame;
-protected:
+ protected:
   bool loaded;
  public:
-  virtual void render(int x, int y, SDL_Rect *clip, SDL_Renderer *renderer) = 0;
+  /*Carga todos los frames de la animacion.
+   * PRE: Debe ser implementada por las clases que hereden de esta*/
   virtual void load_frames(SDL_Renderer *gRenderer) = 0;
+
+  /*Renderiza un frame de la animacion.
+   * PRE: Debe ser implementada por las clases que hereden de esta*/
+  virtual void render(int x, int y, SDL_Rect *clip, SDL_Renderer *renderer) = 0;
+
+  /*Reproduce la animacion.
+   * PRE: Debe ser implementada por las clases que hereden de esta*/
   virtual void play(SDL_Renderer *gRenderer, int x, int y) = 0;
 };
 
