b274a72a50c2a817689119c4d840cc7024ef469a
yuquimura <brianzambellitello@gmail.com>
2019-11-20 19:43:16 -0300

    Refactor in Button class. Documentation of Button class methods
    Signed-off-by: yuquimura <brianzambellitello@gmail.com>

--- client/rendering/interfaces/Button.cpp
+++ client/rendering/interfaces/Button.cpp
@@ -1,8 +1,5 @@
-//
-// Created by brian on 10/30/19.
-//
-
 #include <iostream>
+#include <utility>
 #include "Button.h"
 #include "ButtonAnswer.h"
 
@@ -13,72 +10,61 @@ void Button::setPosition(int x, int y) {
 }
 
 void Button::changeColor(int r, int g, int b, int duration) {
-	this->texture->set_color(r, g, b);
-	colorChangeDuration = duration;
+  this->texture->set_color(r, g, b);
+  colorChangeDuration = duration;
 }
 
 bool Button::handleEvent(SDL_Event *e, std::queue<std::string> *sq) {
-  //If mouse event happened
-
   if (e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP) {
-		//Get mouse position
-		int x, y;
-		SDL_GetMouseState(&x, &y);
-
-		//Check if mouse is in button
-		bool inside = true;
-
-		//Mouse is left of the button
-		if (x < mPosition.x) {
-			inside = false;
-		}
-			//Mouse is right of the button
-		else if (x > mPosition.x + this->area.w) {
-			inside = false;
-		}
-			//Mouse above the button
-		else if (y < mPosition.y) {
-			inside = false;
-		}
-			//Mouse below the button
-		else if (y > mPosition.y + this->area.h) {
-			inside = false;
-		}
-
-		//Mouse is outside button
-		if (!inside) {
-		}
-			//Mouse is inside button
-		else {
-			//Set mouse over sprite
-			switch (e->type) {
-				case SDL_MOUSEMOTION:;
-					return false;
-				case SDL_MOUSEBUTTONDOWN:;
-					sq->push(this->soundWhenPressed);
-					callCallbackFunctions();
-					return true;
-				case SDL_MOUSEBUTTONUP:;
-					return false;
-			}
-		}
+	//Get mouse position
+	int x, y;
+	SDL_GetMouseState(&x, &y);
+	bool inside = true;
+	//Mouse is left of the button
+	if (x < mPosition.x) {
+	  inside = false;
 	}
-	return false;
+	  //Mouse is right of the button
+	else if (x > mPosition.x + this->area.w) {
+	  inside = false;
+	}
+	  //Mouse above the button
+	else if (y < mPosition.y) {
+	  inside = false;
+	}
+	  //Mouse below the button
+	else if (y > mPosition.y + this->area.h) {
+	  inside = false;
+	}
+	if (inside) {
+	  //Set mouse over sprite
+	  switch (e->type) {
+		case SDL_MOUSEMOTION | SDL_MOUSEBUTTONUP:;
+		  return false;
+		case SDL_MOUSEBUTTONDOWN:;
+		  sq->push(this->soundWhenPressed);
+		  callCallbackFunctions();
+		  return true;
+	  }
+	}
+  }
+  return false;
 }
 void Button::render(float screenWidth, float screenHeight) {
   if (colorChangeDuration > -1) colorChangeDuration--;
-	if (colorChangeDuration == 0) changeColor(255, 255, 255, -1);
-  float widthFactor = float(screenWidth / oldWidth);
-  float heightFactor = float(screenHeight / oldHeight);
+  if (colorChangeDuration == 0) changeColor(255, 255, 255, -1);
+  auto widthFactor = float(screenWidth / oldWidth);
+  auto heightFactor = float(screenHeight / oldHeight);
   if (widthFactor != 1) {
-		this->area.x *= widthFactor;
-		this->area.y *= heightFactor;
-		this->mPosition.x = this->area.x;
-		this->mPosition.y = this->area.y;
-		oldWidth = screenWidth;
-		oldHeight = screenHeight;
+	this->area.x *= widthFactor;
+	this->area.y *= heightFactor;
+	this->mPosition.x = this->area.x;
+	this->mPosition.y = this->area.y;
+	oldWidth = screenWidth;
+	oldHeight = screenHeight;
   }
-  texture->render_with_size(area.x, area.y, 0, gRenderer, area.w, area.h, false);
+  texture->render_with_size(area.x, area.y, 0, gRenderer,
+							area.w, area.h, false);
 }
 
 void Button::set_area(int x, int y) {
@@ -87,22 +73,23 @@ void Button::set_area(int x, int y) {
 
 void Button::callCallbackFunctions() {
   for (auto &function : callbacks) {
-		function(this->id);
-	}
-	std::cout << this->id << std::endl;
+	function(this->id);
+  }
+  std::cout << this->id << std::endl;
 }
 
-void Button::addCallbackFunction(std::function<void(std::string)> cf) {
-	callbacks.push_back(cf);
+void Button::addCallbackFunction(const std::function<void(std::string)> &cf) {
+  callbacks.push_back(cf);
 }
 
-Button::Button(std::string id, SDL_Renderer *sdl_renderer, LTexture *buttonSpriteSheet) {
-	mPosition.x = 0;
-	mPosition.y = 0;
-	colorChangeDuration = -1;
-	this->texture = buttonSpriteSheet;
-	this->gRenderer = sdl_renderer;
-  this->id = id;
+Button::Button(std::string id, SDL_Renderer *sdl_renderer,
+			   LTexture *buttonSpriteSheet) {
+  mPosition.x = 0;
+  mPosition.y = 0;
+  colorChangeDuration = -1;
+  this->texture = buttonSpriteSheet;
+  this->gRenderer = sdl_renderer;
+  this->id = std::move(id);
   oldWidth = SCREEN_WIDTH;
   oldHeight = SCREEN_HEIGHT;
 }
--- client/rendering/interfaces/Button.h
+++ client/rendering/interfaces/Button.h
@@ -1,7 +1,3 @@
-//
-// Created by brian on 10/30/19.
-//
-
 #ifndef MICROMACHINES_CLIENT_RENDERING_INTERFACES_BUTTON_H_
 #define MICROMACHINES_CLIENT_RENDERING_INTERFACES_BUTTON_H_
 
@@ -31,19 +27,42 @@ class Button {
 	std::string soundWhenPressed;
 
 	/*Constructor de la clase Button.*/
-	explicit Button(std::string id, SDL_Renderer *sdl_renderer, LTexture *buttonSpriteSheet);
+	explicit Button(std::string id, SDL_Renderer *sdl_renderer,
+					LTexture *buttonSpriteSheet);
 
-	//POS adds a function that will be called when the button is clicked;
-	void addCallbackFunction(std::function<void(std::string)> cf);
+  /*Establece la posicion del boton.
+   * PRE: El origen de coordenadas se encuentra en el extremo superior
+   * izquierdo*/
+  virtual void setPosition(int x, int y);
+
+  /*Agrega una funcion al boton que se ejecutara al momento de clickear el
+   * boton
+   * POST: La nueva funcion, no sobreescribe las anteriores.*/
+  void addCallbackFunction(const std::function<void(std::string)> &cf);
+
+  /*Ejecuta todas las funciones almacenadas en el boton por medio de la
+   * funcion
+   * addCallbackFunction()*/
 	void callCallbackFunctions();
-	virtual void setPosition(int x, int y);
 
-	//Handles mouse event. If the button is clicked, returns true
+  /*Filtra los eventos para quedarse solamente con aquellos del tipo
+   * SDL_MOUSEMOTION,SDL_MOUSEBUTTONDOWN y SDL_MOUSEBUTTONUP. En el caso del
+   * 2do, devuelve true y ejecutacallCallbackFunctions(); En el resto de los
+   * casos, solo devuelve false;*/
 	virtual bool handleEvent(SDL_Event *e, std::queue<std::string> *soundQueue);
 
+  /*Setea el area que sera representara el espacio que ocupara el boton en
+   * pantalla. PRE: tanto x como y deben ser mayores que 0 para su correcto
+   * funcionamiento*/
+	virtual void set_area(int x, int y);
+
+  /*Renderiza el boton con todos los efectos correspondientes.
+   * PRE: screenWidth y screenHeight deben ser el ancho y largo de la pantalla
+   * correspondientes al momento de ejecutar esta funcion. Si alguno de los
+   * dos parametros difiere de la resolucion original, se utilizaran para
+   * recalcular la posicion en la nueva resolucion.*/
   virtual void render(float screenWidth, float screenHeight);
 
-	virtual void set_area(int x, int y);
 
 	//POS apply a color(r,g,b) change to the button for duration frames
 	//if duration is negative then the change persists until called again
