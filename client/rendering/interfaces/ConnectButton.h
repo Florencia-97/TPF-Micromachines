fad55adf51743730ebe9a527fbc115f95b849a64
yuquimura <brianzambellitello@gmail.com>
2019-11-20 19:56:58 -0300

    Delete of unused method in ConnectButton. Documentation
    Signed-off-by: yuquimura <brianzambellitello@gmail.com>

--- client/rendering/interfaces/ConnectButton.h
+++ client/rendering/interfaces/ConnectButton.h
@@ -1,16 +1,15 @@
-//
-// Created by brian on 11/2/19.
-//
-
 #ifndef MICROMACHINES_CLIENT_RENDERING_INTERFACES_CONNECTBUTTON_H_
 #define MICROMACHINES_CLIENT_RENDERING_INTERFACES_CONNECTBUTTON_H_
 #include "Button.h"
+
 class ConnectButton : public Button {
  public:
+  /*Constructor de la clase ConnectButton*/
   ConnectButton(SDL_Renderer *sdl_renderer, LTexture *buttonSpriteSheet);
-  void set_area(int x, int y) override;
-  void render(float screenWidth, float screenHeight) override;
 
+  /*Sobreescritura de la funcion set_area de la clase Button. En esta version,
+   * se modifica que constantes se usan para el calculo*/
+  void set_area(int x, int y) override;
 };
 
 #endif //MICROMACHINES_CLIENT_RENDERING_INTERFACES_CONNECTBUTTON_H_
