8a1647d000809b359684eb5da96f2c07d22dd720
yuquimura <brianzambellitello@gmail.com>
2019-11-22 18:51:59 -0300

    Clean of LTexture
    Signed-off-by: yuquimura <brianzambellitello@gmail.com>

--- client/rendering/LTexture.cpp
+++ client/rendering/LTexture.cpp
@@ -14,24 +14,26 @@ LTexture::~LTexture() {
 }
 
 /*Loads a texture from the path, using the renderer*/
-bool LTexture::load_from_file(const std::string &path, SDL_Renderer *renderer) {
+bool LTexture::load_from_file(const std::string &path,
+							  SDL_Renderer *renderer) {
   free();
   SDL_Texture *newTexture = nullptr;
   SDL_Surface *loadedSurface = IMG_Load(path.c_str());
   if (loadedSurface == nullptr) {
-    printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+	printf("Unable to load image %s! SDL_image Error: %s\n",
+		   path.c_str(), IMG_GetError());
   } else {
-    SDL_SetColorKey(loadedSurface,
-                    SDL_TRUE,
-                    SDL_MapRGB(loadedSurface->format,0, 0xFF,0xFF));
-    newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-    if (newTexture == nullptr) {
-      printf("Unable to create texture from %s! SDL_Error: %s\n", path.c_str(), SDL_GetError());
-    } else {
-      width = loadedSurface->w;
-      height = loadedSurface->h;
-    }
-    SDL_FreeSurface(loadedSurface);
+	SDL_SetColorKey(loadedSurface,
+					SDL_TRUE,
+					SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+	newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+	if (newTexture == nullptr) {
+	  printf("Unable to create texture from %s! SDL_Error: %s\n", path.c_str(), SDL_GetError());
+	} else {
+	  width = loadedSurface->w;
+	  height = loadedSurface->h;
+	}
+	SDL_FreeSurface(loadedSurface);
   }
   texture = newTexture;
   texture_name = path;
@@ -41,33 +43,26 @@ bool LTexture::load_from_file(const std::string &path, SDL_Renderer *renderer) {
 /*Resets the LTexture*/
 void LTexture::free() {
   if (texture != nullptr) {
-    SDL_DestroyTexture(texture);
-    texture = nullptr;
-    width = 0;
-    height = 0;
+	SDL_DestroyTexture(texture);
+	texture = nullptr;
+	width = 0;
+	height = 0;
   }
 }
 
 /* Modules the color in the texture*/
-void LTexture::set_color(Uint8 red,
-                         Uint8 green,
-                         Uint8 blue) {
-  SDL_SetTextureColorMod(texture,
-                         red,
-                         green,
-                         blue);
+void LTexture::set_color(Uint8 red, Uint8 green, Uint8 blue) {
+  SDL_SetTextureColorMod(texture, red, green, blue);
 }
 
-
-
-void LTexture::render(int x, int y, SDL_Rect *clip, double angle, SDL_Point *center,
-                        SDL_RendererFlip flip, SDL_Renderer *renderer) {
+void LTexture::render(int x, int y, SDL_Rect *clip, double angle,
+					  SDL_Point *center, SDL_RendererFlip flip, SDL_Renderer *renderer) {
   SDL_Rect renderQuad = {x, y, width, height};
   if (clip != nullptr) {
-    renderQuad.w = clip->w;
-    renderQuad.h = clip->h;
+	renderQuad.w = clip->w;
+	renderQuad.h = clip->h;
   }
-  SDL_RenderCopyEx(renderer,texture, clip, &renderQuad, angle, center, flip);
+  SDL_RenderCopyEx(renderer, texture, clip, &renderQuad, angle, center, flip);
 
 }
 
@@ -81,43 +76,40 @@ int LTexture::getWidth() {
 SDL_Texture *LTexture::get_texture() {
   return this->texture;
 }
-void LTexture::render_with_size(float x,
-                                float y,
-                                int r,
-                                SDL_Renderer *renderer,
-                                int widthParam,
-                                int heightParam,
-                                bool id) {
+void LTexture::render_with_size(float x, float y, int r, SDL_Renderer *renderer,
+								int widthParam, int heightParam, bool id) {
   if (!id) {
-    SDL_Rect area = {static_cast<int>(x), static_cast<int>(y), widthParam, heightParam};
-    SDL_RenderCopyEx(renderer, texture, nullptr, &area, r, nullptr, SDL_FLIP_NONE);
+	SDL_Rect area = {static_cast<int>(x), static_cast<int>(y),
+					 widthParam, heightParam};
+	SDL_RenderCopyEx(renderer, texture, nullptr, &area, r,
+					 nullptr, SDL_FLIP_NONE);
   } else {
-    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+	SDL_RenderCopy(renderer, texture, nullptr, nullptr);
   }
 }
-void LTexture::loadFromRenderedText(const std::string &msg, SDL_Color color, TTF_Font *font, SDL_Renderer *renderer) {
+void LTexture::loadFromRenderedText(const std::string &msg, SDL_Color color,
+									TTF_Font *font, SDL_Renderer *renderer) {
   free();
   //Render text surface
   SDL_Surface *textSurface = TTF_RenderText_Solid(font, msg.c_str(), color);
   if (textSurface != nullptr) {
-    //Create texture from surface pixels
-    texture = SDL_CreateTextureFromSurface(renderer, textSurface);
-    if (texture == nullptr) {
-      printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
-    } else {
-      //Get image dimensions
-      width = textSurface->w;
-      height = textSurface->h;
-    }
+	//Create texture from surface pixels
+	texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+	if (texture == nullptr) {
+	  printf("Unable to create texture from rendered text! SDL Error: %s\n",
+			 SDL_GetError());
+	} else {
+	  //Get image dimensions
+	  width = textSurface->w;
+	  height = textSurface->h;
+	}
 
-    //Get rid of old surface
-    SDL_FreeSurface(textSurface);
+	//Get rid of old surface
+	SDL_FreeSurface(textSurface);
   } else {
-    printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+	printf("Unable to render text surface! SDL_ttf Error: %s\n",
+		   TTF_GetError());
   }
-}
-std::string LTexture::get_string_name() {
-  return texture_name;
 }
 
 void LTexture::setAlpha(Uint8 alpha) {
@@ -125,7 +117,4 @@ void LTexture::setAlpha(Uint8 alpha) {
   SDL_SetTextureAlphaMod(texture, alpha);
 }
 
-bool LTexture::isLoaded() {
-    return texture;
-}
 
