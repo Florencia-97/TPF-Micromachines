2a57d8452749e7ce1efbbca0aaa06e39192e6042
yuquimura <brianzambellitello@gmail.com>
2019-11-23 16:11:53 -0300

    Documentation of TextureLoader.h and TEXTURE_ERROR.h
    Signed-off-by: yuquimura <brianzambellitello@gmail.com>

--- client/rendering/TextureLoader.cpp
+++ client/rendering/TextureLoader.cpp
@@ -1,17 +1,15 @@
-//
-// Created by brian on 10/26/19.
-//
-
 #include "TextureLoader.h"
 #include "TEXTURE_ERROR.h"
 
-LTexture* TextureLoader::load_texture(std::string name, SDL_Renderer *renderer) {
-    if (texture_cache.find(name) == texture_cache.end()){
-        texture_cache.emplace(name, LTexture{});
-        if (!texture_cache[name].load_from_file("client/rendering/assets/"+name, renderer)) {
-            texture_cache.erase(name);
-            throw TEXTURE_ERROR("error loading image\n");
-        }
-    }
-    return &texture_cache[name];
+LTexture *TextureLoader::load_texture(std::string name,
+									  SDL_Renderer *renderer) {
+  if (texture_cache.find(name) == texture_cache.end()) {
+	texture_cache.emplace(name, LTexture{});
+	if (!texture_cache[name].load_from_file("client/rendering/assets/" + name,
+											renderer)) {
+	  texture_cache.erase(name);
+	  throw TEXTURE_ERROR("error loading image\n");
+	}
+  }
+  return &texture_cache[name];
 }
