6b1801bb9a0df7ef337e3635b6cf42d78b6b0bef
Florencia-97 <florrr1997@gmail.com>
2019-11-26 13:48:29 -0300

    Docs for fakeClient

--- client/ia/FakeClient.h
+++ client/ia/FakeClient.h
@@ -17,12 +17,17 @@ class FakeClient : public BaseThread {
     ThreadQueue* soundQueue;
     LuaWrapper luaWrapper;
     MapsLayer mapsLayer;
+    // Fake client starts moving foward
     void _firstMove();
+    // Gets the id of the car we must control
     std::string _getId();
+    // Loop for each we get scripts desition and push it to sender queue
     void _run() override;
+    // Gets move of script
     bool _move(InfoBlock& ib, int x, int y, int r, int& lastMove);
 public:
     FakeClient(SafeQueue<InfoBlock> &kq, std::queue<InfoBlock> &pos, ThreadQueue *sq);
+    // Sets the race we are going to run
     void setRace(std::string raceName);
 };
 
